44b9374f73eba563c32e219ad049240f39c8f425
wise-integer-6834 <ghaxhaj@fordham.edu>
2020-01-05 21:56:10 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,11 @@ def read_from_hash(hash, key)
    hash[key]
 end
 
-def update_counting_hash(hash, key)
-  hash[key] ? hash[key] +=1 : hash[key] = 1
-  hash
-end
+def update_counting_hash(hash,key)
+if hash[key] == nil
+  hash[key] = 1
+else
+  hash[key] += 1
+ end
+ hash
+ end
